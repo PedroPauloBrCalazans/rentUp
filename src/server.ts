d9af512bb68dd7ccdc0a9d5be0c08e7e3656f21a
Pedro <pedropaulo91.calazans@gmail.com>
2022-10-26 16:22:23 -0300

    configurações do projeto

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    return response.json({message: "Ola!"})
+});
+
+app.post("/curso", (request, response) => {
+    const { name } = request.body;
+
+    return response.json({ name });
+})
+
+app.listen(3333, () => console.log("Subiu!"));
