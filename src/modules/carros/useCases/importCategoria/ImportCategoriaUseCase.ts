6b0d4e36e12c109edd62385132fdd8d813d45fe2
Pedro <pedropaulo91.calazans@gmail.com>
2023-04-12 21:10:02 -0300

    nova lib para leitura de arquivos via uploads CSV + Implementações no useCase e Controller.

--- src/modules/carros/useCases/importCategoria/ImportCategoriaUseCase.ts
+++ src/modules/carros/useCases/importCategoria/ImportCategoriaUseCase.ts
@@ -1,12 +1,57 @@
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
+import { ICategoriasRepository } from "../../repositories/ICategoriasRepository";
 
-
+//array para todas as informações da categorias
+interface IImportCategoria {
+    nome: string;
+    descricao: string;
+}
 
 class ImportCategoriaUseCase {
+    constructor(private categoriasRepository: ICategoriasRepository) {}
 
-    execute(file: any) {
-        
-    }
+    loadCategorias(file: Express.Multer.File): Promise<IImportCategoria[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categorias: IImportCategoria[] = [];
     
+            const parseFile = csvParse();
+    
+            stream.pipe(parseFile);
+    
+            parseFile.on("data", async (line) => {
+                const [nome, descricao] = line
+                categorias.push({
+                    nome,
+                    descricao,
+                });
+            })
+            .on("end", () => {
+                resolve(categorias);
+            })
+            .on("error", (err) => {
+                reject(err);
+            });
+        });
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categorias = await this.loadCategorias(file);
+        
+        categorias.map(async (categoria) => {
+            const { nome, descricao } = categoria;
+
+            const existeCategoria = this.categoriasRepository.verificarCategoriaDuplicada(nome);
+
+            if(!existeCategoria) {
+                this.categoriasRepository.createCategoria({
+                    nome,
+                    descricao,
+                });
+            }
+         });
+    }
 }
 
 export { ImportCategoriaUseCase };
--- src/modules/carros/useCases/importCategoria/index.ts
+++ src/modules/carros/useCases/importCategoria/index.ts
@@ -1,8 +1,9 @@
+import { CategoriasRepository } from "../../repositories/implementations/CategoriasRepository";
 import { ImportCategoriaController } from "./ImportCategoriaController";
 import { ImportCategoriaUseCase } from "./ImportCategoriaUseCase";
 
-
-const importCategoriaUseCase = new ImportCategoriaUseCase()
+const categoriasRepository = CategoriasRepository.getInstance();
+const importCategoriaUseCase = new ImportCategoriaUseCase(categoriasRepository);
 const importCategoriaController = new ImportCategoriaController(importCategoriaUseCase);
 
-export {importCategoriaController}
+export {importCategoriaController};
