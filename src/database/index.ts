679f0fc5eb030814f76665b093130956cf02ebe7
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-04-22 16:17:03 -0300

    novas lib (typeOrm + postgres) + configurações para iniciar o BD.

---
+++ src/database/index.ts
@@ -0,0 +1,9 @@
+import { DataSource } from "typeorm";
+
+const dataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    username: "",
+    password: "",
+    database: "",
+});
